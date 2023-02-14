36c3aab37b71cc9a0dccd96a7018b47d2881fc35
hexskrt <90503141+hexskrt@users.noreply.github.com>
2023-02-14 09:29:10 +0700

    Update testnet-ordos.sh

--- testnet-ordos.sh
+++ testnet-ordos.sh
@@ -10,53 +10,44 @@ echo "  ██   ██ ██       ██ ██  ██  ██ ██ ██
 echo "  ██   ██ ███████ ██   ██ ██   ████  ██████  ██████  ███████ ███████";
 echo "Cosmovisor Automatic Installer for Game Of Terra Alliance chain id ordosd";
 echo -e "\e[0m"
-
 sleep 1
 
 # Variable
-TESTCORE_WALLET=wallet
-TESTCORE=cored
-BINARY=cosmovisor
-TESTCORE_ID=coreum-testnet-1
-TESTCORE_FOLDER=.core
-TESTCORE_VER=v0.1.1
-TESTCORE_BINARY=https://github.com/CoreumFoundation/coreum/releases/download
-TESTCORE_BIN=cored-linux-amd64
-TESTCORE_GENESIS=https://snap.nodexcapital.com/coreum/genesis.json
-TESTCORE_ADDRBOOK=https://snap.nodexcapital.com/coreum/addrbook.json
-TESTCORE_DENOM=utestcore
-TESTCORE_PORT=21
+ORDOS_WALLET=wallet
+ORDOS=ordosd
+ORDOS_ID=ordos-1
+ORDOS_FOLDER=.ordos
+ORDOS_VER=v0.0.1-goa
+ORDOS_REPO=https://github.com/terra-money/alliance
+ORDOS_DENOM=uord
+ORDOS_PORT=01
 
-echo "export TESTCORE_WALLET=${TESTCORE_WALLET}" >> $HOME/.bash_profile
-echo "export TESTCORE=${TESTCORE}" >> $HOME/.bash_profile
-echo "export BINARY=${BINARY}" >> $HOME/.bash_profile
-echo "export TESTCORE_ID=${TESTCORE_ID}" >> $HOME/.bash_profile
-echo "export TESTCORE_FOLDER=${TESTCORE_FOLDER}" >> $HOME/.bash_profile
-echo "export TESTCORE_VER=${TESTCORE_VER}" >> $HOME/.bash_profile
-echo "export TESTCORE_BINARY=${TESTCORE_BINARY}" >> $HOME/.bash_profile
-echo "export TESTCORE_BIN=${TESTCORE_BIN}" >> $HOME/.bash_profile
-echo "export TESTCORE_GENESIS=${TESTCORE_GENESIS}" >> $HOME/.bash_profile
-echo "export TESTCORE_ADDRBOOK=${TESTCORE_ADDRBOOK}" >> $HOME/.bash_profile
-echo "export TESTCORE_DENOM=${TESTCORE_DENOM}" >> $HOME/.bash_profile
-echo "export TESTCORE_PORT=${TESTCORE_PORT}" >> $HOME/.bash_profile
+echo "export ORDOS_WALLET=${ORDOS_WALLET}" >> $HOME/.bash_profile
+echo "export ORDOS=${ORDOS}" >> $HOME/.bash_profile
+echo "export ORDOS_ID=${ORDOS_ID}" >> $HOME/.bash_profile
+echo "export ORDOS_FOLDER=${ORDOS_FOLDER}" >> $HOME/.bash_profile
+echo "export ORDOS_VER=${ORDOS_VER}" >> $HOME/.bash_profile
+echo "export ORDOS_REPO=${ORDOS_REPO}" >> $HOME/.bash_profile
+echo "export ORDOS_DENOM=${ORDOS_DENOM}" >> $HOME/.bash_profile
+echo "export ORDOS_PORT=${ORDOS_PORT}" >> $HOME/.bash_profile
 source $HOME/.bash_profile
 
 # Set Vars
-if [ ! $TESTCORE_NODENAME ]; then
-        read -p "sxlzptprjkt@w00t666w00t:~# [ENTER YOUR NODE] > " TESTCORE_NODENAME
-        echo 'export TESTCORE_NODENAME='$TESTCORE_NODENAME >> $HOME/.bash_profile
+if [ ! $ORDOS_NODENAME ]; then
+        read -p "sxlzptprjkt@w00t666w00t:~# [ENTER YOUR NODE] > " ORDOS_NODENAME
+        echo 'export ORDOS_NODENAME='$ORDOS_NODENAME >> $HOME/.bash_profile
 fi
 echo ""
-echo -e "YOUR NODE NAME : \e[1m\e[31m$TESTCORE_NODENAME\e[0m"
-echo -e "NODE CHAIN ID  : \e[1m\e[31m$TESTCORE_ID\e[0m"
-echo -e "NODE PORT      : \e[1m\e[31m$TESTCORE_PORT\e[0m"
+echo -e "YOUR NODE NAME : \e[1m\e[31m$ORDOS_NODENAME\e[0m"
+echo -e "NODE CHAIN ID  : \e[1m\e[31m$ORDOS_ID\e[0m"
+echo -e "NODE PORT      : \e[1m\e[31m$ORDOS_PORT\e[0m"
 echo ""
 
 # Update
 sudo apt update && sudo apt upgrade -y
 
 # Package
-sudo apt install curl build-essential jq chrony lz4 -y
+sudo apt install make build-essential gcc git jq chrony lz4 -y
 
 # Install GO
 ver="1.19.5"
@@ -69,96 +60,95 @@ echo "export PATH=$PATH:/usr/local/go/bin:$HOME/go/bin" >> ~/.bash_profile
 source ~/.bash_profile
 go version
 
-# Get testnet version of coreum
+# Get testnet version of alliance (terra)
 cd $HOME
-curl -Ls $TESTCORE_BINARY/$TESTCORE_VER/$TESTCORE_BIN > $TESTCORE
-chmod +x $TESTCORE
-go install cosmossdk.io/tools/cosmovisor/cmd/cosmovisor@v1.4.0
-
-# Prepare binaries for Cosmovisor
-mkdir -p $HOME/$TESTCORE_FOLDER/$TESTCORE_ID/$BINARY/genesis/bin
-mv $TESTCORE $HOME/$TESTCORE_FOLDER/$TESTCORE_ID/$BINARY/genesis/bin/
-
-# Create application symlinks
-ln -s $HOME/$TESTCORE_FOLDER/$TESTCORE_ID/$BINARY/genesis $HOME/$TESTCORE_FOLDER/$TESTCORE_ID/$BINARY/current
-sudo ln -s $HOME/$TESTCORE_FOLDER/$TESTCORE_ID/$BINARY/current/bin/$TESTCORE /usr/bin/$TESTCORE
-
-# Create Service
-sudo tee /etc/systemd/system/$TESTCORE.service > /dev/null << EOF
-[Unit]
-Description=$BINARY
-After=network-online.target
-
-[Service]
-User=$USER
-ExecStart=$(which $BINARY) run start
-Restart=on-failure
-RestartSec=10
-LimitNOFILE=65535
-Environment="DAEMON_HOME=$HOME/$TESTCORE_FOLDER/$TESTCORE_ID"
-Environment="DAEMON_NAME=$TESTCORE"
-Environment="UNSAFE_SKIP_BACKUP=true"
-
-[Install]
-WantedBy=multi-user.target
-EOF
-
-# Register Service
-sudo systemctl daemon-reload
-sudo systemctl enable $TESTCORE
+rm -rf alliance
+git clone $ORDOS_REPO
+cd alliance
+git checkout $ORDOS_VER
+make build-alliance ACC_PREFIX=ordos
+sudo mv build/$ORDOS /usr/bin/
 
 # Init generation
-$TESTCORE config chain-id $TESTCORE_ID
-$TESTCORE config keyring-backend file
-$TESTCORE config node tcp://localhost:${TESTCORE_PORT}657
-$TESTCORE init $TESTCORE_NODENAME --chain-id $TESTCORE_ID
+$ORDOS config chain-id $ORDOS_ID
+$ORDOS config keyring-backend file
+$ORDOS config node tcp://localhost:${ORDOS_PORT}657
+$ORDOS init $ORDOS_NODENAME --chain-id $ORDOS_ID
 
 # Set peers and seeds
-PEERS="479773376706c0643289a365e84e440cced10bb9@146.190.81.135:21656,4b8d541efbb343effa1b5079de0b17d2566ac0fd@34.172.70.24:26656,051a07f1018cfdd6c24bebb3094179a6ceda2482@138.201.123.234:26656,1a3a573c53a4b90ab04eb47d160f4d3d6aa58000@35.233.117.165:26656,39a34cd4f1e908a88a726b2444c6a407f67e4229@158.160.59.199:26656,b2978432c0126f28a6be7d62892f8ded1e48d227@34.70.241.13:26656,7c0d4ce5ad561c3453e2e837d85c9745b76f7972@35.238.77.191:26656,5add70ec357311d07d10a730b4ec25107399e83c@5.196.7.58:26656,27450dc5adcebc84ccd831b42fcd73cb69970881@35.239.146.40:26656,abbeb588ad88176a8d7592cd8706ebbf7ef20cfe@185.241.151.197:26656,69d7028b7b3c40f64ea43208ecdd43e88c797fd6@34.69.126.231:26656,0aa5fa2507ada8a555d156920c0b09f0d633b0f9@34.173.227.148:26656"
-SEEDS="64391878009b8804d90fda13805e45041f492155@35.232.157.206:26656,53f2367d8f8291af8e3b6ca60efded0675ff6314@34.29.15.170:26656"
-sed -i -e "s|^persistent_peers *=.*|persistent_peers = \"$PEERS\"|" $HOME/$TESTCORE_FOLDER/$TESTCORE_ID/config/config.toml
-sed -i -e "s|^seeds *=.*|seeds = \"$SEEDS\"|" $HOME/$TESTCORE_FOLDER/$TESTCORE_ID/config/config.toml
+PEERS="3f636d4bc99a309797bbaef5934fc9c3b8f3070c@146.190.81.135:01656"
+SEEDS="1772a7a48530cc8adc447fdb7b720c064411667b@goa-seeds.lavenderfive.com:11656"
+sed -i -e "s|^persistent_peers *=.*|persistent_peers = \"$PEERS\"|" $HOME/$ORDOS_FOLDER/config/config.toml
+sed -i -e "s|^seeds *=.*|seeds = \"$SEEDS\"|" $HOME/$ORDOS_FOLDER/config/config.toml
 
-# Download genesis and addrbook
-curl -Ls $TESTCORE_GENESIS > $HOME/$TESTCORE_FOLDER/$TESTCORE_ID/config/genesis.json
-curl -Ls $TESTCORE_ADDRBOOK > $HOME/$TESTCORE_FOLDER/$TESTCORE_ID/config/addrbook.json
+# Create file genesis.json
+touch $HOME/$ORDOS_FOLDER/config/genesis.json
 
 # Set Port
-sed -i.bak -e "s%^proxy_app = \"tcp://127.0.0.1:26658\"%proxy_app = \"tcp://127.0.0.1:${TESTCORE_PORT}658\"%; s%^laddr = \"tcp://127.0.0.1:26657\"%laddr = \"tcp://127.0.0.1:${TESTCORE_PORT}657\"%; s%^pprof_laddr = \"localhost:6060\"%pprof_laddr = \"localhost:${TESTCORE_PORT}060\"%; s%^laddr = \"tcp://0.0.0.0:26656\"%laddr = \"tcp://0.0.0.0:${TESTCORE_PORT}656\"%; s%^prometheus_listen_addr = \":26660\"%prometheus_listen_addr = \":${TESTCORE_PORT}660\"%" $HOME/$TESTCORE_FOLDER/$TESTCORE_ID/config/config.toml
-sed -i.bak -e "s%^address = \"tcp://0.0.0.0:1317\"%address = \"tcp://0.0.0.0:${TESTCORE_PORT}317\"%; s%^address = \":8080\"%address = \":${TESTCORE_PORT}080\"%; s%^address = \"0.0.0.0:9090\"%address = \"0.0.0.0:${TESTCORE_PORT}090\"%; s%^address = \"0.0.0.0:9091\"%address = \"0.0.0.0:${TESTCORE_PORT}091\"%" $HOME/$TESTCORE_FOLDER/$TESTCORE_ID/config/app.toml
+sed -i.bak -e "s%^proxy_app = \"tcp://127.0.0.1:26658\"%proxy_app = \"tcp://127.0.0.1:${ORDOS_PORT}658\"%; s%^laddr = \"tcp://127.0.0.1:26657\"%laddr = \"tcp://127.0.0.1:${ORDOS_PORT}657\"%; s%^pprof_laddr = \"localhost:6060\"%pprof_laddr = \"localhost:${ORDOS_PORT}060\"%; s%^laddr = \"tcp://0.0.0.0:26656\"%laddr = \"tcp://0.0.0.0:${ORDOS_PORT}656\"%; s%^prometheus_listen_addr = \":26660\"%prometheus_listen_addr = \":${ORDOS_PORT}660\"%" $HOME/$ORDOS_FOLDER/config/config.toml
+sed -i.bak -e "s%^address = \"tcp://0.0.0.0:1317\"%address = \"tcp://0.0.0.0:${ORDOS_PORT}317\"%; s%^address = \":8080\"%address = \":${ORDOS_PORT}080\"%; s%^address = \"0.0.0.0:9090\"%address = \"0.0.0.0:${ORDOS_PORT}090\"%; s%^address = \"0.0.0.0:9091\"%address = \"0.0.0.0:${ORDOS_PORT}091\"%" $HOME/$ORDOS_FOLDER/config/app.toml
 
 # Set Config Pruning
 pruning="custom"
 pruning_keep_recent="100"
 pruning_keep_every="0"
-pruning_interval="19"
-sed -i -e "s/^pruning *=.*/pruning = \"$pruning\"/" $HOME/$TESTCORE_FOLDER/$TESTCORE_ID/config/app.toml
-sed -i -e "s/^pruning-keep-recent *=.*/pruning-keep-recent = \"$pruning_keep_recent\"/" $HOME/$TESTCORE_FOLDER/$TESTCORE_ID/config/app.toml
-sed -i -e "s/^pruning-keep-every *=.*/pruning-keep-every = \"$pruning_keep_every\"/" $HOME/$TESTCORE_FOLDER/$TESTCORE_ID/config/app.toml
-sed -i -e "s/^pruning-interval *=.*/pruning-interval = \"$pruning_interval\"/" $HOME/$TESTCORE_FOLDER/$TESTCORE_ID/config/app.toml
+pruning_interval="50"
+sed -i -e "s/^pruning *=.*/pruning = \"$pruning\"/" $HOME/$ORDOS_FOLDER/config/app.toml
+sed -i -e "s/^pruning-keep-recent *=.*/pruning-keep-recent = \"$pruning_keep_recent\"/" $HOME/$ORDOS_FOLDER/config/app.toml
+sed -i -e "s/^pruning-keep-every *=.*/pruning-keep-every = \"$pruning_keep_every\"/" $HOME/$ORDOS_FOLDER/config/app.toml
+sed -i -e "s/^pruning-interval *=.*/pruning-interval = \"$pruning_interval\"/" $HOME/$ORDOS_FOLDER/config/app.toml
 
 # Set minimum gas price
-sed -i -e "s/^minimum-gas-prices *=.*/minimum-gas-prices = \"0$TESTCORE_DENOM\"/" $HOME/$TESTCORE_FOLDER/$TESTCORE_ID/config/app.toml
+sed -i -e "s/^minimum-gas-prices *=.*/minimum-gas-prices = \"0.001$ORDOS_DENOM\"/" $HOME/$ORDOS_FOLDER/config/app.toml
 
-# Enable snapshots
-$TESTCORE tendermint unsafe-reset-all --keep-addr-book
-rm -rf $HOME/$TESTCORE_FOLDER/$TESTCORE_ID/data
-wget https://snap.nodexcapital.com/coreum/coreum-latest.tar.lz4
-lz4 coreum-latest.tar.lz4
-tar -xvf coreum-latest.tar
-mv $TESTCORE_ID/data $HOME/$TESTCORE_FOLDER/$TESTCORE_ID
-rm -f coreum-latest.tar.lz4
-rm -f coreum-latest.tar
-rm -rf coreum-testnet-1
+# Set config snapshot
+sed -i -e "s/^snapshot-interval *=.*/snapshot-interval = \"1000\"/" $HOME/$ORDOS_FOLDER/config/app.toml
+sed -i -e "s/^snapshot-keep-recent *=.*/snapshot-keep-recent = \"2\"/" $HOME/$ORDOS_FOLDER/config/app.toml
 
-#Start Service
-sudo systemctl start $TESTCORE
+# Enable state sync
+$ORDOS tendermint unsafe-reset-all --home $HOME/$ORDOS_FOLDER
+
+SNAP_RPC="https://rpc-goa-ordos.sxlzptprjkt.xyz:443"
+
+LATEST_HEIGHT=$(curl -s $SNAP_RPC/block | jq -r .result.block.header.height); \
+BLOCK_HEIGHT=$((LATEST_HEIGHT - 2000)); \
+TRUST_HASH=$(curl -s "$SNAP_RPC/block?height=$BLOCK_HEIGHT" | jq -r .result.block_id.hash)
+
+echo ""
+echo -e "\e[1m\e[31m[!]\e[0m HEIGHT : \e[1m\e[31m$LATEST_HEIGHT\e[0m BLOCK : \e[1m\e[31m$BLOCK_HEIGHT\e[0m HASH : \e[1m\e[31m$TRUST_HASH\e[0m"
+echo ""
+
+sed -i.bak -E "s|^(enable[[:space:]]+=[[:space:]]+).*$|\1true| ; \
+s|^(rpc_servers[[:space:]]+=[[:space:]]+).*$|\1\"$SNAP_RPC,$SNAP_RPC\"| ; \
+s|^(trust_height[[:space:]]+=[[:space:]]+).*$|\1$BLOCK_HEIGHT| ; \
+s|^(trust_hash[[:space:]]+=[[:space:]]+).*$|\1\"$TRUST_HASH\"|" $HOME/$ORDOS_FOLDER/config/config.toml
+
+# Create Service
+sudo tee /etc/systemd/system/$ORDOS.service > /dev/null <<EOF
+[Unit]
+Description=$ORDOS
+After=network-online.target
+
+[Service]
+User=$USER
+ExecStart=$(which $ORDOS) start --home $HOME/$ORDOS_FOLDER
+Restart=on-failure
+RestartSec=3
+LimitNOFILE=4096
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+# Register And Start Service
+sudo systemctl daemon-reload
+sudo systemctl enable $ORDOS
+sudo systemctl start $ORDOS
 
 echo -e "\e[1m\e[31mSETUP FINISHED\e[0m"
 echo ""
-echo -e "CHECK STATUS BINARY : \e[1m\e[31msystemctl status $TESTCORE\e[0m"
-echo -e "CHECK RUNNING LOGS  : \e[1m\e[31mjournalctl -fu $TESTCORE -o cat\e[0m"
-echo -e "CHECK LOCAL STATUS  : \e[1m\e[31mcurl -s localhost:${TESTCORE_PORT}657/status | jq .result.sync_info\e[0m"
+echo -e "CHECK RUNNING LOGS : \e[1m\e[31mjournalctl -fu $ORDOS -o cat\e[0m"
+echo -e "CHECK LOCAL STATUS : \e[1m\e[31mcurl -s localhost:${ORDOS_PORT}657/status | jq .result.sync_info\e[0m"
 echo ""
 
 # End
